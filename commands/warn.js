25e382d8d0f587de0a0683017638fc1f8a92efe9
NotCryingCloud <61647304+Jeksin@users.noreply.github.com>
2021-07-28 13:00:35 +0300

    будущие планы

--- commands/warn.js
+++ commands/warn.js
@@ -1,6 +1,8 @@
 module.exports = {
     name: 'warn',
     async run(client, message, args) {
-        
+        const { errorPermissions } = require('../errors.js')
+
+        if(!message.author.hasPermission("BAN_MEMBER")) return message.channel.send(errorPermissions)
     }
 }
---
+++ errors.js
@@ -0,0 +1,4 @@
+const errorPermissions = new Discord.MessageEmbed()
+            .setColor('RED')
+            .setTitle('Ошибка!')
+            .setDescription('У вас недостаточно прав!')
