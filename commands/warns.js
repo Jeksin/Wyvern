c840dcfc30a000e4c6cf201b72470d103f46b6a5
NotCryingCloud <61647304+Jeksin@users.noreply.github.com>
2021-08-02 01:39:09 +0300

    warn system(in dev)#2

--- commands/warns.js
+++ commands/warns.js
@@ -1,14 +1,16 @@
 module.exports = {
     name: 'warns',
     async run (client, message, args) {
+        const db = require('quick.db');
         const user = message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.author;
 
         let warnings = await db.get(`warnings_${message.guild.id}_${user.id}`);
+        let reason = await db.get(`reason_${message.guild.id}_${user.id}`)
 
         if(warnings === null) warnings = 0;
         
         
-        db.get(`reason_${message.guild.id}_${user.id}`, reason);
+        //db.get(`reason_${message.guild.id}_${user.id}`, reason);
         message.channel.send(`**${user.username}** has *${warnings}* warning the reasons is ${reason}`);
     }
 }
